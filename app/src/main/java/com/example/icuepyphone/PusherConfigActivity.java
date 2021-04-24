81371fd1534aa3c137e78c019141ceac9bcab72f
ScreamingOranges <daniel.ksel@gmail.com>
2021-04-23 20:14:29 -0400

    Removed unused imports

--- app/src/main/java/com/example/icuepyphone/PusherConfigActivity.java
+++ app/src/main/java/com/example/icuepyphone/PusherConfigActivity.java
@@ -2,11 +2,9 @@ package com.example.icuepyphone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
