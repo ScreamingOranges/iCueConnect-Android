3782102dbeb2a730e979f1d012b0c49da15b6401
ScreamingOranges <daniel.ksel@gmail.com>
2021-05-04 22:24:59 -0400

    Fixed duplicate pusher calls for single notification

--- app/src/main/java/com/example/icuepyphone/NotificationListener.java
+++ app/src/main/java/com/example/icuepyphone/NotificationListener.java
@@ -1,21 +1,36 @@
 package com.example.icuepyphone;
 
+import android.app.Notification;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 
 public class NotificationListener extends NotificationListenerService {
     static InterfaceNotificationListener INL;
-
+    private String previousApp;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        previousApp = "";
     }
 
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        previousApp = "";
+    }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
-        INL.setValue(sbn.getPackageName());
+        if ((sbn.getNotification().flags & Notification.FLAG_GROUP_SUMMARY) != 0) {
+            return;
+        }
+        else if(previousApp.equals(sbn.getPackageName())){
+            return;
+        }
+        else{
+            previousApp = sbn.getPackageName();
+            INL.setValue(sbn.getPackageName());
+        }
     }
 
 
