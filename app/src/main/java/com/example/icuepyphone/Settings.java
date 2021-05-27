f48d1129b0ff74825fe779862314b8a50f866f60
ScreamingOranges <daniel.ksel@gmail.com>
2021-05-26 20:54:58 -0400

    replaced cluster input field with dropdown

--- app/src/main/java/com/example/icuepyphone/Settings.java
+++ app/src/main/java/com/example/icuepyphone/Settings.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import com.example.icuepyphone.databinding.ActivitySettingsBinding;
@@ -30,6 +31,9 @@ public class Settings extends AppCompatActivity {
         setContentView(view);
         context = getApplicationContext();
         mDatabaseHelper = new DatabaseHelper(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.cluster_spinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.clusterSpinner.setAdapter(adapter);
 
         Cursor data = mDatabaseHelper.getData();
         if((data != null) && (data.getCount() > 0)){
@@ -43,7 +47,7 @@ public class Settings extends AppCompatActivity {
             binding.inputAppId.setText(listData.get(0));
             binding.inputKey.setText(listData.get(1));
             binding.inputSecret.setText(listData.get(2));
-            binding.inputCluster.setText(listData.get(3));
+            binding.clusterSpinner.setSelection(adapter.getPosition(listData.get(3)));
         }
 
 
@@ -53,7 +57,7 @@ public class Settings extends AppCompatActivity {
                 app_id = binding.inputAppId.getText().toString();
                 key = binding.inputKey.getText().toString();
                 secret = binding.inputSecret.getText().toString();
-                cluster = binding.inputCluster.getText().toString();
+                cluster = binding.clusterSpinner.getSelectedItem().toString();
                 if(app_id.isEmpty()||key.isEmpty()||secret.isEmpty()||cluster.isEmpty()){
                     Toast.makeText(context, "Please Fill In All Fields.", Toast.LENGTH_SHORT).show();
                 }
