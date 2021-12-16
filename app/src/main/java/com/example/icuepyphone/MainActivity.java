3ecbfb9a4890a351cf23f8d8081e69516f9c5d1c
ScreamingOranges <daniel.ksel@gmail.com>
2021-12-15 22:40:07 -0500

    Code Clean Up And Bug Fix For Queued Toasts And Button Color Reset

--- app/src/main/java/com/example/icuepyphone/MainActivity.java
+++ app/src/main/java/com/example/icuepyphone/MainActivity.java
@@ -16,14 +16,12 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.icuepyphone.databinding.ActivityMainBinding;
-import java.util.Map;
 import top.defaults.colorpicker.ColorObserver;
 
 public class MainActivity extends AppCompatActivity implements InterfaceNotificationListener{
     private ActivityMainBinding binding;
+    //Global toast to track if a toast is set at a given moment
     private Toast msg;
-    private Map<String,String> devices;
-    private String inputRGB;
     private Context context;
     private int DefaultColor;
     private int DeviceIndex;
@@ -79,7 +77,7 @@ public class MainActivity extends AppCompatActivity implements InterfaceNotifica
         pusherHelper = new PusherHelper(MainActivity.this);
 
         //Assigns map to spinner
-        Utility.assignSpinner(devices, this, binding);
+        Utility.assignSpinner(null, this, binding);
         //request devices connected to iCUE from API
         requestDeviceHelper(this);
 
@@ -118,23 +116,27 @@ public class MainActivity extends AppCompatActivity implements InterfaceNotifica
         binding.buttonUpdateiCUE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                inputRGB = binding.inputRGBVal.getText().toString();
+                String inputRGB = binding.inputRGBVal.getText().toString();
                 String[] result = inputRGB.split("\\s+");
                 if((inputRGB.isEmpty()) && (DefaultColor == 0) ){
-                    Toast.makeText(context, "ENTER/CHOOSE AN RGB VALUE", Toast.LENGTH_SHORT).show();
+                    if (msg != null) { msg.cancel(); }
+                    msg = Toast.makeText(context, "ENTER/CHOOSE AN RGB VALUE", Toast.LENGTH_SHORT);
+                    msg.show();
                 }
                 else if(!inputRGB.isEmpty()) {
                     if(!inputRGB.matches("\\d{1,3}\\s+\\d{1,3}\\s+\\d{1,3}")){
                         binding.inputRGBVal.setText("");
-                        Toast.makeText(context, "Improper Input Format!", Toast.LENGTH_SHORT).show();
+                        if (msg != null) { msg.cancel(); }
+                        msg = Toast.makeText(context, "Improper Input Format!", Toast.LENGTH_SHORT);
+                        msg.show();
                         return;
                     }
                     pusherHelper.trigger(context, DeviceIndex, Integer.parseInt(result[0]), Integer.parseInt(result[1]), Integer.parseInt(result[2]));
                 }
                 else{
                     pusherHelper.trigger(context, DeviceIndex, Color.red(DefaultColor), Color.green(DefaultColor), Color.blue(DefaultColor));
-                    DefaultColor = 0;
                 }
+                DefaultColor = 0;
                 binding.previewSelectedColor.setBackgroundColor(-5592406);
                 binding.inputRGBVal.setText("");
             }
@@ -149,12 +151,12 @@ public class MainActivity extends AppCompatActivity implements InterfaceNotifica
     //Helps preform request for devices connected to iCUE from API
     public void requestDeviceHelper(Context context){
         if(!pusherHelper.pusherCredentials.isEmpty()){
-            if (null != msg) { msg.cancel(); }
+            if (msg != null) { msg.cancel(); }
             msg = Toast.makeText(context, "Requesting Devices From API", Toast.LENGTH_SHORT);
             new requestDeviceHandler().execute(context);
         }
         else{
-            if(null != msg){ msg.cancel(); }
+            if(msg != null){ msg.cancel(); }
             msg = Toast.makeText(context, "CONFIGURE PUSHER IN SETTINGS", Toast.LENGTH_SHORT);
         }
         msg.show();
